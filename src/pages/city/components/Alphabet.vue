218d28818fda1754996eea8d60ccbe847428fe40
kidskey <zai021@163.com>
2018-07-31 17:59:29 +0800

    city ajax

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,12 +1,15 @@
 <template>
     <ul class="list">
-        <li class="item">A</li>
+        <li class="item" v-for="(item,key) in ct" :key="key">{{key}}</li>
     </ul>
 </template>
 <script>
 
     export default{
         name:'CityAlphabet',
+        props:{
+            ct: Object
+        },
         data(){
             return {
 
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -7,55 +7,22 @@
                     <div class="button-wrapper">
                         <div class="button">北京</div>
                     </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
                 </div>
             </div>
             <div class="area">
                 <div class="title border-topbottom">热门城市</div>
                 <div class="button-list">
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
-                    </div>
-                    <div class="button-wrapper">
-                        <div class="button">北京</div>
+                    <div class="button-wrapper" v-for="item in hct" :key="item.id">
+                        <div class="button">{{item.name}}</div>
                     </div>
                 </div>
             </div>
-            <div class="area">
-                <div class="title border-topbottom">A</div>
+            <div class="area" v-for="(item,key) in ct" :key="key">
+                <div class="title border-topbottom">{{key}}</div>
                 <ul class="item-list">
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
-                    <li class="item border-bottom">阿拉尔</li>
+                    <li class="item border-bottom" v-for="innerItem in item" :key="innerItem.id">
+                        {{innerItem.name}}
+                    </li>
                 </ul>
             </div>
         </div>
@@ -67,6 +34,10 @@
 
     export default{
         name:'CityList',
+        props:{
+            ct: Object,
+            hct: Array
+        },
         data(){
             return {
 
