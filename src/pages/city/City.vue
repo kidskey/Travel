c9e53c26020ae88dde01a580a6b07d104692a661
kidskey <zai021@163.com>
2018-08-01 18:22:11 +0800

    performance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list :ct="cities" :hct="hotCities"></city-list>
-        <city-alphabet :ct="cities"></city-alphabet>
+        <city-list :ct="cities" :hct="hotCities" :letter="letter"></city-list>
+        <city-alphabet :ct="cities" @change="handleChange"></city-alphabet>
     </div>
 </template>
 <script>
@@ -17,7 +17,8 @@
         data(){
             return {
                 cities:{},
-                hotCities:[]
+                hotCities:[],
+                letter:''
             }
         },
         components:{
@@ -42,6 +43,9 @@
                     this.cities = data.cities
                     this.hotCities = data.hotCities
                 }
+            },
+            handleChange(letter){
+                this.letter = letter;
             }
         }
     }
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,7 +17,9 @@
                     </div>
                 </div>
             </div>
-            <div class="area" v-for="(item,key) in ct" :key="key">
+            <div class="area" 
+                v-for="(item,key) in ct" 
+                :key="key" :ref="key">
                 <div class="title border-topbottom">{{key}}</div>
                 <ul class="item-list">
                     <li class="item border-bottom" v-for="innerItem in item" :key="innerItem.id">
@@ -36,7 +38,8 @@
         name:'CityList',
         props:{
             ct: Object,
-            hct: Array
+            hct: Array,
+            letter: String
         },
         data(){
             return {
@@ -45,6 +48,14 @@
         },
         mounted(){
             this.scroll = new BScroll(this.$refs.wrapper)
+        },
+        watch:{
+            letter(){
+                if(this.letter){
+                    const element = this.$refs[this.letter][0]
+                    this.scroll.scrollToElement(element)
+                }
+            }
         }
     }
 </script>
